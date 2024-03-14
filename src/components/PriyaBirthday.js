4a80360ff602460c05d85cc5028fbda391b00b54
Vimal <vimalkumarak478@gmail.com>
2024-03-14 22:41:48 +0530

    first commit

---
+++ src/components/PriyaBirthday.js
@@ -0,0 +1,23 @@
+import React from 'react';
+// import './BirthdayWish.css'; // Import CSS file for styling
+
+const BirthdayWish = () => {
+    return (
+        <div className="birthday-wish">
+            <h1>Happy Birthday to My Amazing Girlfriend! 🎉❤️💃</h1>
+            <div className="heart-animation">
+            <span className="heart">&#128150;</span>
+            <span role="img" aria-label="dancing-girl">💃</span>
+            </div>
+            <p>Dear Priya Rajput(Devi Ji),</p>
+            <p>On this special day, I want to take a moment to express just how much you mean to me. You bring so much joy, love, and happiness into my life every single day, and I am incredibly grateful to have you by my side.</p>
+            <p>Your kindness, compassion, and unwavering support inspire me to be a better person, and I cherish every moment that we spend together.</p>
+            <p>Today, I hope all of your birthday wishes come true because you deserve nothing but the absolute best. May this year be filled with endless love, laughter, and unforgettable memories.</p>
+            <p>Happy birthday, my love! Here's to many more wonderful years together.</p>
+            <p>With all my love,</p>
+            <p>Vimal Kumar Chaudhary</p>
+        </div>
+    );
+};
+
+export default BirthdayWish;
